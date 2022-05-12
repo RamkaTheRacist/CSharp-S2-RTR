173200c5f44c15f61365e7cdcefba342dfcff76b
Ramka <whereisourgod@gmail.com>
2022-05-12 11:43:04 +0300

    translated

--- task3/Program.cs
+++ task3/Program.cs
@@ -3,9 +3,9 @@ Console.WriteLine("Input number");
 int number = int.Parse(Console.ReadLine());
 if(number / 10000 == number % 10 && (number / 1000 % 10) == (number % 100 / 10))
 {
-    Console.WriteLine($"{number} является палиндромом");
+    Console.WriteLine($"{number} is palindrome");
 }
 else
 {
-    Console.WriteLine($"{number} не является палиндромом");
+    Console.WriteLine($"{number} isnt palindrome");
 }
