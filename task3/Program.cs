e5282e965b4ed86e186bfc1809918a693abd9286
Ramka <whereisourgod@gmail.com>
2022-05-12 11:37:48 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Программа проверяет пятизначное число на палиндромом.
+Console.WriteLine("Input number");
+int number = int.Parse(Console.ReadLine());
+if(number / 10000 == number % 10 && (number / 1000 % 10) == (number % 100 / 10))
+{
+    Console.WriteLine($"{number} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"{number} не является палиндромом");
+}
