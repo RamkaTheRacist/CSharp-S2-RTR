7621722bb00751d7c244394e3bd7f7e9c93a9391
Ramka <whereisourgod@gmail.com>
2022-05-12 10:27:46 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Дано число. Проверить кратно ли оно 7 и 23
+Console.WriteLine("Insert ur number");
+int number = int.Parse(Console.ReadLine());
+if(number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"{number} is multiple of 7 and 23");
+}
+else
+{
+    Console.WriteLine($"{number} isnt multiple of 7 and 23");
+}
