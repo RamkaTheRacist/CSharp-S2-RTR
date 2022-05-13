76d755e76bace36838699817e783cbe4ca615f2e
Ramka <whereisourgod@gmail.com>
2022-05-13 04:43:48 +0300

    HW

---
+++ HW/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Which day of week?");
+int number = int.Parse(Console.ReadLine());
+if(number < 6 && number > 0)
+{
+    Console.WriteLine("This day isnt weekend");
+
+}
+else if(number == 6 || number == 7)
+{
+    Console.WriteLine("This day is weekend");
+}
+else
+{
+    Console.WriteLine("We have only 7 days in week");
+}
